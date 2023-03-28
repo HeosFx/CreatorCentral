c8720e3b5681ed5fe69e6ac3bfdbf8968eb10346
maxime-bl <maxime.blanchard19@gmail.com>
2023-03-28 16:03:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Creator Central</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+</body>
+</html>
+
 <?php
+
+?>
