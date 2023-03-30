e972e8985c0e0664cbdeba3b0d04b932174a5cfc
maxime-bl <maxime.blanchard19@gmail.com>
2023-03-30 14:58:39 +0200

    Début HTML+CSS homepage (image de fond)

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Creator Central</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./styles/background-image.css">
+    <link rel="stylesheet" href="./styles/homepage.css">
 </head>
 
 <body>
+<div class="bg-image1">
+    <div class="wrapper">
+        <div class="header">
+            <h1>Guten Tag</h1>
 
+            <p>Ceci est un test</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
 
