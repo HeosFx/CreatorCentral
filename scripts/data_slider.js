02c776a746d432cd3f1520c5a1ff29d5ff71ebe4
HeosFx <82534081+HeosFx@users.noreply.github.com>
2023-04-21 20:17:05 +0200

    Ajout du layer de data sur la page d'accueil

---
+++ scripts/data_slider.js
@@ -0,0 +1,42 @@
+/* Variable that stock the last slide shown on the screen */
+let last_slide = 1;
+
+/*
+Function used to show the data corresponding to the first slide
+ */
+function showData1() {
+    let str = "slide" + last_slide + "-container";
+    document.getElementById(str).classList.remove('show');
+    document.getElementById('slide1-container').classList.add('show');
+    last_slide = 1;
+}
+
+/*
+Function used to show the data corresponding to the second slide
+ */
+function showData2() {
+    let str = "slide" + last_slide + "-container";
+    document.getElementById(str).classList.remove('show');
+    document.getElementById('slide2-container').classList.add('show');
+    last_slide = 2;
+}
+
+/*
+Function used to show the data corresponding to the third slide
+ */
+function showData3() {
+    let str = "slide" + last_slide + "-container";
+    document.getElementById(str).classList.remove('show');
+    document.getElementById('slide3-container').classList.add('show');
+    last_slide = 3;
+}
+
+/*
+Function used to show the data corresponding to the fourth slide
+ */
+function showData4() {
+    let str = "slide" + last_slide + "-container";
+    document.getElementById(str).classList.remove('show');
+    document.getElementById('slide4-container').classList.add('show');
+    last_slide = 4;
+}
